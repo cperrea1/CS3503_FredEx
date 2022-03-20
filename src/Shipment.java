d6a93d963b4a2bb2fef3365e179cd1506bfc4eea
Courtney Perreault <courtneyperreault@Courtneys-MacBook-Air.local>
2022-03-20 12:07:00 -0300

    some sample instance varaiables for shipment object

--- src/Shipment.java
+++ src/Shipment.java
@@ -1,3 +1,16 @@
 public class Shipment {
 
+    private int shipmentID;
+    private int status;
+    /*
+     * status codes:
+     * 0 = shipment received/entered in system
+     * 1 = shipment on truck
+     * 2 = shipment out for delivery
+     * 3 = delivered
+     * 4 = exception
+     */
+    private Recipient receipient;
+    private Sender sender;
+    private Address deliveryAddress;
 }
